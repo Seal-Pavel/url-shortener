2aa2503f6c4a75a1632b2f2d74e1a0d52feaac23
seal <osipov.pv1000@gmail.com>
2023-01-02 19:09:08 +0400

    Предотвращение состояния гонки

--- app/polls/views.py
+++ app/polls/views.py
@@ -20,7 +20,7 @@ def detail(request, question_id):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        selected_choice = question.choice_set.filter(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
@@ -28,8 +28,7 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.votes += 1
-        selected_choice.save()
+        selected_choice.update(votes=F('votes') + 1)
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
