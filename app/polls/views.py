17a126f21834ca810dfa62b4f0cf1fd1b9ceae39
seal <osipov.pv1000@gmail.com>
2023-01-02 18:46:42 +0400

    reset

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.db.models import F
 
 from .models import Question, Choice
 
